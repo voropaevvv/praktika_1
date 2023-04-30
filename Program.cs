939c7fd2195f1d5d71c7ea0102926f4d6d70f12b
voropaevvv <132216943+voropaevvv@users.noreply.github.com>
2023-04-30 19:51:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+
+
+namespace mathexamtask
+
+{
+
+    internal class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("Type the x: ");
+
+            string text = Console.ReadLine();
+
+            double x = double.Parse(text);
+
+
+
+            if (x < 4)
+
+            {
+
+                double y = (Math.Pow(x, 2) / 6) + (0.5);
+
+                Console.WriteLine("Y: " + y);
+
+            }
+
+            else if (x >= 4 && x < 10)
+
+            {
+
+                double pi = Math.PI;
+
+                for (int i = 4; i <= x; i++)
+
+                {
+
+                    double y = (0.25) * Math.Pow(x, 3);
+
+                    Console.WriteLine("Y: " + y * pi);
+
+                }
+
+            }
+
+            else
+
+            {
+
+                double summ = 0;
+
+                double y = 0;
+
+                for (int i = 4; i <= x; i++)
+
+                {
+
+                    summ += i * i;
+
+                    y = 0.5 * summ * 3.5 * i;
+
+                }
+
+                Console.WriteLine("Y:" + y);
+
+            }
+
+
+
+        }
+
+    }
+
+}
